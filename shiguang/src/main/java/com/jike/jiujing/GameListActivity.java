90a9a055e8aad4ba36a4caab17861537697d507d
sjgb8d <Zhiyuan1_Wang.sgmwh@saic-gm.com>
2020-10-22 11:03:54 +0800

    游戏介绍

--- shiguang/src/main/java/com/jike/jiujing/GameListActivity.java
+++ shiguang/src/main/java/com/jike/jiujing/GameListActivity.java
@@ -19,8 +19,10 @@ import java.util.Map;
 public class GameListActivity extends EuclidActivity {
 
     public static final String PARAM_TITLE = "title";
+    public static String gameTitle;
 
     public static void go(Context context, String title) {
+        gameTitle=title;
         Intent intent = new Intent(context, GameListActivity.class);
         intent.putExtra(PARAM_TITLE, title);
         context.startActivity(intent);
@@ -29,6 +31,7 @@ public class GameListActivity extends EuclidActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //gameTitle=getIntent().getStringExtra(PARAM_TITLE);
     }
 
     @Override
@@ -44,19 +47,68 @@ public class GameListActivity extends EuclidActivity {
                 R.drawable.game5,
                 R.drawable.game6,
                 R.drawable.game7,
+                R.drawable.game8,
+                R.drawable.game9,
+                R.drawable.game10,
+                R.drawable.game11,
+                R.drawable.game12,
+                R.drawable.game13,
+                R.drawable.game14,
+                R.drawable.game15,
+                R.drawable.game16,
+                R.drawable.game17,
               };
         String[] names = getResources().getStringArray(R.array.array_names);
         String[] shorts=getResources().getStringArray(R.array.array_shorts);
         String[] longs=getResources().getStringArray(R.array.array_longs);
 
-        for (int i = 0; i < avatars.length; i++) {
+        if (gameTitle.equals("怀旧系列")){
+            for (int i = 0; i < 7; i++) {
+                profileMap = new HashMap<>();
+                profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
+                profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, shorts[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, longs[i]);
+                profilesList.add(profileMap);
+            }
+        }else if (gameTitle.equals("电竞系列")){
+            for (int i = 7; i < 11; i++) {
+                profileMap = new HashMap<>();
+                profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
+                profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, shorts[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, longs[i]);
+                profilesList.add(profileMap);
+            }
+        }else if (gameTitle.equals("抖音挑战系列")){
+            for (int i = 11; i < 16; i++) {
+                profileMap = new HashMap<>();
+                profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
+                profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, shorts[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, longs[i]);
+                profilesList.add(profileMap);
+            }
+        }else {
+            for (int i = 16; i < 17; i++) {
+                profileMap = new HashMap<>();
+                profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
+                profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, shorts[i]);
+                profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, longs[i]);
+                profilesList.add(profileMap);
+            }
+
+        }
+
+     /*   for (int i = 0; i < avatars.length; i++) {
             profileMap = new HashMap<>();
             profileMap.put(EuclidListAdapter.KEY_AVATAR, avatars[i]);
             profileMap.put(EuclidListAdapter.KEY_NAME, names[i]);
             profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_SHORT, shorts[i]);
             profileMap.put(EuclidListAdapter.KEY_DESCRIPTION_FULL, longs[i]);
             profilesList.add(profileMap);
-        }
+        }*/
 
         return new EuclidListAdapter(this, R.layout.euclid_list_item, profilesList);
     }
